27408a56a4c25c4308ae59cb3c3e593b33a3489d
cenek.jansa <cenek.jansa@anycoders.cz>
2026-02-03 18:36:30 +0100

    fis: upload script

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -1,7 +1,7 @@
 curl -v -X POST http://localhost:8080/graphql \
   -H "Content-Type: multipart/form-data" \
   -F operations='{
-    "query": "mutation UploadDoc($id: Int!, $file: Upload!) { uploadDocument(sectionId: $id, file: $file) { success, attachmentId, errorMessage } }",
+    "query": "mutation UploadDoc($id: ID!, $file: Upload!) { uploadDocument(sectionId: $id, file: $file) { success, attachmentId, errorMessage } }",
     "variables": {
       "id": 1,
       "file": null 
